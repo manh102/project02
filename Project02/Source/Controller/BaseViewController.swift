0f3c3ea8f5918a427eff5fac25a00c5b2ad1645e
Saphir Thermopolis <manhtt102@gmail.com>
2020-04-07 13:46:30 +0700

    init commit

---
+++ Project02/Source/Controller/BaseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BaseViewController.swift
+//  Project02
+//
+//  Created by Dat Nguyen on 4/7/20.
+//  Copyright © 2020 Manh Tran. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Project02/Source/ViewModel/LoginViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewModel.swift
+//  Project02
+//
+//  Created by Manh Tran on 4/7/20.
+//  Copyright © 2020 Manh Tran. All rights reserved.
+//
+
+import Foundation
+
+class LoginViewModel: BaseViewModel {
+    
+}
